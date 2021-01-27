94417dd0ad361a97d3d6d69e96439fee9ea26a6c
Ben <ben.basque@icloud.com>
2021-01-26 23:49:00 -0500

    added images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import profile from './components/profile'
 import withAuth from './components/withAuth'
 import login from './components/login'
 import post from './components/post'
+import image from './components/image'
 import './App.css';
 
 function App() {
@@ -14,12 +15,14 @@ function App() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/profile">Profile</Link></li>
             <li><Link to="/post">Post</Link></li>
+            <li><Link to="/image">Image</Link></li>
             <li><Link to="/login">Login</Link></li>
           </ul>
           <Switch>
             <Route path="/" exact component={home} />
             <Route path="/profile" component={withAuth(profile)} />
             <Route path="/post" component={withAuth(post)} />
+            <Route path="/image" component={withAuth(image)} />
             <Route path="/login" component={login} />
           </Switch>
       </Router>
